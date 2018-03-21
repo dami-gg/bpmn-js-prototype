7e7d9ae831844c4dc2a4b786c83e09e6a564e295
Damián García <damian.garcia@zalando.de>
2018-03-21 14:34:07 +0100

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# bpmn-js prototype
-
-Prototype of usage of [bpmn-js library](https://bpmn.io/toolkit/bpmn-js/)
--- src/vendor/index.js
+++
@@ -1,5 +0,0 @@
-import "bpmn-js/dist/assets/bpmn-font/css/bpmn.css";
-import "bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css";
-import "bpmn-js/dist/assets/bpmn-font/css/bpmn-codes.css";
-import "bpmn-js/dist/assets/diagram-js.css";
-import "./bpmn-js-properties-panel.css";
